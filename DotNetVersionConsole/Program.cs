10c4aa16124b1de337dbe57f3a34e735557fe56a
gavin <gavin>
2014-01-23 01:30:41 -0800

    init
    copy-pasted code from stackoverflow ftw!
    http://stackoverflow.com/a/2451408/975935

---
+++ DotNetVersionConsole/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+
+
+namespace DotNetVersionConsole
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The following .NET Framework versions are installed:\n");
+            foreach (Version ver in InstalledDotNetVersions())
+                Console.WriteLine(ver);
+
+            Console.ReadKey();
+        }
+
+
+        public static Collection<Version> InstalledDotNetVersions()
+        {
+            Collection<Version> versions = new Collection<Version>();
+            RegistryKey NDPKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP");
+            if (NDPKey != null)
+            {
+                string[] subkeys = NDPKey.GetSubKeyNames();
+                foreach (string subkey in subkeys)
+                {
+                    GetDotNetVersion(NDPKey.OpenSubKey(subkey), subkey, versions);
+                    GetDotNetVersion(NDPKey.OpenSubKey(subkey).OpenSubKey("Client"), subkey, versions);
+                    GetDotNetVersion(NDPKey.OpenSubKey(subkey).OpenSubKey("Full"), subkey, versions);
+                }
+            }
+            return versions;
+        }
+
+        private static void GetDotNetVersion(RegistryKey parentKey, string subVersionName, Collection<Version> versions)
+        {
+            if (parentKey != null)
+            {
+                string installed = Convert.ToString(parentKey.GetValue("Install"));
+                if (installed == "1")
+                {
+                    string version = Convert.ToString(parentKey.GetValue("Version"));
+                    if (string.IsNullOrEmpty(version))
+                    {
+                        if (subVersionName.StartsWith("v"))
+                            version = subVersionName.Substring(1);
+                        else
+                            version = subVersionName;
+                    }
+
+                    Version ver = new Version(version);
+
+                    if (!versions.Contains(ver))
+                        versions.Add(ver);
+                }
+            }
+        }
+    }
+}
