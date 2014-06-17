9028b0f9b0dd6905ecae3b6a46b2402e5fa8c8ac
gavinws <gavinws>
2014-06-16 22:12:41 -0700

    overhaul for tabular output and .net 4.5+ versions

--- DotNetVersionConsole/Program.cs
+++ DotNetVersionConsole/Program.cs
@@ -13,50 +13,97 @@ namespace DotNetVersionConsole
         static void Main(string[] args)
         {
             Console.WriteLine("The following .NET Framework versions are installed:\n");
-            foreach (Version ver in InstalledDotNetVersions())
-                Console.WriteLine(ver);
-
+            Console.WriteLine(String.Format("|{0,12}|{1,16}|{2,5}|{3,7}|{4,14}|", "Name", "Version", "SP", "Release", "4.5.x Version"));
+            Console.WriteLine(new string('-', 60));
+            GetVersionFromRegistry();
+            Console.WriteLine(new string('-', 60));
+            Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
 
-
-        public static Collection<Version> InstalledDotNetVersions()
+        private static int Get45or451FromRegistry()
         {
-            Collection<Version> versions = new Collection<Version>();
-            RegistryKey NDPKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP");
-            if (NDPKey != null)
+            using (RegistryKey ndpKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\"))
             {
-                string[] subkeys = NDPKey.GetSubKeyNames();
-                foreach (string subkey in subkeys)
+                int releaseKey = (int)ndpKey.GetValue("Release");
                 {
-                    GetDotNetVersion(NDPKey.OpenSubKey(subkey), subkey, versions);
-                    GetDotNetVersion(NDPKey.OpenSubKey(subkey).OpenSubKey("Client"), subkey, versions);
-                    GetDotNetVersion(NDPKey.OpenSubKey(subkey).OpenSubKey("Full"), subkey, versions);
+                    return releaseKey;
                 }
             }
-            return versions;
         }
 
-        private static void GetDotNetVersion(RegistryKey parentKey, string subVersionName, Collection<Version> versions)
+        private static void GetVersionFromRegistry()
         {
-            if (parentKey != null)
+        using (RegistryKey ndpKey = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "").OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))        
             {
-                string installed = Convert.ToString(parentKey.GetValue("Install"));
-                if (installed == "1")
+                foreach (string versionKeyName in ndpKey.GetSubKeyNames())
                 {
-                    string version = Convert.ToString(parentKey.GetValue("Version"));
-                    if (string.IsNullOrEmpty(version))
+                    if (versionKeyName.StartsWith("v"))
                     {
-                        if (subVersionName.StartsWith("v"))
-                            version = subVersionName.Substring(1);
+                        RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
+                        string name = (string)versionKey.GetValue("Version", "");
+                        string sp = versionKey.GetValue("SP", "").ToString();
+                        string install = versionKey.GetValue("Install", "").ToString();
+                        string releaseKey = "";
+                        string net45xVer = "";
+                        if (name.StartsWith("4.5"))
+                        {
+                            int releaseKeyNumber = Get45or451FromRegistry();
+                            releaseKey = Get45or451FromRegistry().ToString();
+                            if (releaseKeyNumber == 378675 | releaseKeyNumber == 378675)
+                                net45xVer = "4.5.1";
+                            if (releaseKeyNumber == 379893)
+                                net45xVer = "4.5.2";
+                            if (releaseKeyNumber > 379893)
+                                net45xVer = "4.5.2+";
+                        }
+                                                    
+                        if (install == "") //no install info, must be later.                            
+                            Console.WriteLine(String.Format("|{0,12}|{1,16}|{2,5}|{3,7}|{4,14}|", versionKeyName, name, sp, releaseKey, net45xVer));
                         else
-                            version = subVersionName;
+                        {
+                            if (sp != "" && install == "1")
+                            {
+                                Console.WriteLine(String.Format("|{0,12}|{1,16}|{2,5}|{3,7}|{4,14}|", versionKeyName, name, sp, releaseKey, net45xVer));                             
+                            }
+                        }
+                        if (name != "")
+                        {
+                            continue;
+                        }
+                        foreach (string subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
+                            name = (string)subKey.GetValue("Version", "");
+                            if (name != "")
+                                sp = subKey.GetValue("SP", "").ToString();
+                            install = subKey.GetValue("Install", "").ToString();
+                            if (name.StartsWith("4.5"))
+                            {
+                                releaseKey = Get45or451FromRegistry().ToString();
+                                int releaseKeyNumber = Get45or451FromRegistry();
+                                if (releaseKeyNumber == 378675 | releaseKeyNumber == 378758)
+                                    net45xVer = "4.5.1";
+                                if (releaseKeyNumber == 379893)
+                                    net45xVer = "4.5.2";
+                                if (releaseKeyNumber > 379893)
+                                    net45xVer = "4.5.2+";
+                            }
+                            if (install == "") //no install info, must be later.
+                                Console.WriteLine(String.Format("|{0,12}|{1,16}|{2,5}|{3,7}|{4,14}|", versionKeyName, name, sp, releaseKey, net45xVer));
+                            else
+                            {
+                                if (sp != "" && install == "1")
+                                {
+                                    Console.WriteLine(String.Format("|{0,12}|{1,16}|{2,5}|{3,7}|{4,14}|", subKeyName, name, sp, releaseKey, net45xVer));
+                                }
+                                else if (install == "1")
+                                {
+                                    Console.WriteLine(String.Format("|{0,12}|{1,16}|{2,5}|{3,7}|{4,14}|", subKeyName, name, sp, releaseKey, net45xVer));
+                                }
+                            }
+                        }
                     }
-
-                    Version ver = new Version(version);
-
-                    if (!versions.Contains(ver))
-                        versions.Add(ver);
                 }
             }
         }
